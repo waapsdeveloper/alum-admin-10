5ec78d87c674ba253af9eba203719e84415abe6d
Waapsdeveloper <waapsdeveloper@gmail.com>
2024-06-01 15:37:43 +0500

    updates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_ENV') !== 'local') {
+        if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
     }
