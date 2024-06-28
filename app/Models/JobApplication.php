c2da8334abc3011e8e520045c089b81f3b4016c6
Waapsdeveloper <waapsdeveloper@gmail.com>
2024-06-28 19:44:30 +0500

    JobApplication

---
+++ app/Models/JobApplication.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JobApplication extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = [
+        'user_id',
+        'job_id',
+        'name',
+        'phone_number',
+        'cv'
+    ];
+}
